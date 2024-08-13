842a21b1488df7706b419a8994504d8e0819c413
joheiss <josef.heiss@jovisco.de>
2024-08-13 15:03:02 +0200

    #3

---
+++ src/main/java/com/jovisco/springsecurity/oauth2social/Oauth2socialApplication.java
@@ -0,0 +1,13 @@
+package com.jovisco.springsecurity.oauth2social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2socialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2socialApplication.class, args);
+	}
+
+}
